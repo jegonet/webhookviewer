c74460ac7a9e394426a65063e84860fa743e3701
MiguelPaezArkiteck <miguel.paez@arkiteck.com>
2019-03-15 09:08:45 -0500

    Se agrego El Body y el Headers para observar el Hook

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.get('*', (req, res) => {
 })
 
 // This responds a POST request for the homepage
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
+app.post('*', function (req, res) {
+   console.log(req.headers);
+   console.log(req.body);
    res.send('Hello POST');
 })
 
