6bcc21c7134055e0ed1b0157cf8d9b6a46f0a141
MiguelPaezArkiteck <miguel.paez@arkiteck.com>
2019-03-15 11:05:52 -0500

    Quitar Carpeta de Modulos

--- index.js
+++ index.js
@@ -24,9 +24,3 @@ app.post('*', function (req, res) {
 })
 
 module.exports = app
-//var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-
-//    console.log("Example app listening at http://%s:%s", host, port)
-//})
