70999cc8a567da555ae6f11a76c4b7b6e580e383
MiguelPaezArkiteck <miguel.paez@arkiteck.com>
2019-03-15 11:00:17 -0500

    se agregro el bodyParser y modifico el send por write

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var bodyParser = require('body-parser')//add this
-app.use(bodyParser())//add this before any route or before using req.body
-
 const express = require('express')
 const app = express()
+
+var bodyParser = require('body-parser')//add this
+app.use(bodyParser())//add this before any route or before using req.body
 
 app.get('*', (req, res) => {
     res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>')
@@ -14,11 +14,19 @@ app.get('*', (req, res) => {
 
 // This responds a POST request for the homepage
 app.post('*', function (req, res) {
+    
    console.log(req.headers);
    console.log(req.body);
 
-   res.send('Hello POST');
+    res.write("Hola");
+    res.end()
 
 })
 
 module.exports = app
+//var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+
+//    console.log("Example app listening at http://%s:%s", host, port)
+//})
